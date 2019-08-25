f135f0a776cde2390fe5c29265d7bddcafe0f65d
BBadoo <bbader0806@gmail.com>
2019-08-25 13:45:39 -0400

    layouting

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="one">1</div>
+      <div className="two">2</div>
+      <div className="three">3</div>
+      <div className="four">4</div>
+      <div className="five">5</div>
+      <div className="six">6</div>
+      <div className="seven">7</div>
+      <div className="eight">8</div>
+      <div className="nine">9</div>
+      <div className="ten">10</div>
+      
+    </div>
+  );
+}
+
+export default App;
